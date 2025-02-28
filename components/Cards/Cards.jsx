69525bf26a95eb15682aa2d83b67432d5aa1fbaf
Imrexxx-OG <imrexxx93@gmail.com>
2025-02-28 12:53:05 +0100

    added cards and testimonials components

--- components/Cards/Cards.jsx
+++ components/Cards/Cards.jsx
@@ -2,8 +2,17 @@ import React from "react"
 
 export default function Cards(){
   return (
-    <div className="cards">
-      <h2 className="sub-header">CARDS</h2>
+    <div>
+      <h1 className="sub-header">CARDS</h1>
+      <div className="card-container">
+        <div className="card">
+          <div className="card-icon-blue">
+            <div className="card-icon-cloud"></div>
+          </div>
+          <h3 className="card-title">Easy Deployment</h3>
+          "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis."
+        </div>
+      </div>
     </div>
   )
 }
--- components/Testimonials/Testimonials.jsx
+++ components/Testimonials/Testimonials.jsx
@@ -3,7 +3,37 @@ import React from "react"
 export default function Testimonials(){
   return (
     <div className="testimonials">
-      <h2 className="sub-header">TESTIMONIALS</h2>
+      <h1 className="sub-header">TESTIMONIALS</h1>
+      <div className="testimonial-container">
+        <h3>With Pic</h3>
+        <div className="testimonial-with-pic">
+          <div className="plate"></div>
+          <img src="" alt="my portrait" className="headshot-image" />
+          <div className="testimonial-with-pic-container">
+            <img src="" alt="open quotation mark" />
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis molestiae iure nulla ad, praesentium deserunt illo amet a dolore sit corporis, culpa velit, optio itaque dignissimos quam veniam porro fugiat."
+            <div className="testimonial-with-pic-signature">
+              <div className="testmonial-with-pic-name">Imran Damare</div>
+              <div className="testmonial-with-pic-title">RootEM, Founder</div>
+            </div>
+          </div>
+        </div>
+        <h3>No Pic</h3>
+        <div className="testimonial-no-pic">
+          <div className="testimonial-no-pic-container">
+            <img src="" alt="RootEM logo" />
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis molestiae iure nulla ad, praesentium deserunt illo amet a dolore sit corporis, culpa velit, optio itaque dignissimos quam veniam porro fugiat.
+            </p>
+            <div className="testimonial-no-pic-signature">
+              <div className="testmonial-no-pic-name">Imran Damare</div>
+              <img src="" alt="divider-image" />
+              <div className="testmonial-no-pic-title">RootEM, Founder</div>
+            </div>
+          </div>
+          <div className="dot-pattern"></div>
+        </div>
+      </div>
     </div>
   )
 }
